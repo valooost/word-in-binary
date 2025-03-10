c6f938c74bb51fe87e4894c2acaacee03a5427bc
valooost <152898127+valooost@users.noreply.github.com>
2025-03-10 14:49:39 +0100

    Add files via upload

---
+++ Encryptor.py
@@ -0,0 +1,22 @@
+def text_to_binary(text):
+    words = text.split()
+    binary_words = []
+    
+    for word in words:
+        binary_letters = []
+        for char in word:
+            if char.isalpha():
+                if char.islower():
+                    pos = ord(char) - ord('a') + 1  # Kleinbuchstaben 1–26
+                else:
+                    pos = ord(char) - ord('A') + 27  # Großbuchstaben 27–52
+                binary_letters.append(bin(pos)[2:])
+        
+        binary_words.append(" ".join(binary_letters))
+    
+    return "  ".join(binary_words)
+
+information = input("Enter text: ")
+print("Binary: ", text_to_binary(information))
+print(" ")
+input("Press "Space" or "Enter" to close.")
